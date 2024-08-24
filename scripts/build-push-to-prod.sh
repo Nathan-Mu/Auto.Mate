3dd4765dfd56a37d6dc8672b3c716b7fd8252768
Nathan <zhaody.mu@gmail.com>
2024-08-24 21:47:00 +1000

    added docker commands

---
+++ scripts/build-push-to-prod.sh
@@ -0,0 +1,5 @@
+version=$(sh ./scripts/get-version.sh)
+echo $version
+docker build -t auto.mate:$version .
+docker tag auto.mate:$version nathan2hao/auto.mate:$version
+docker push nathan2hao/auto.mate:$version
---
+++ scripts/get-version.sh
@@ -0,0 +1,1 @@
+jq -r .version package.json
