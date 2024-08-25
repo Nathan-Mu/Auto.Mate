04281c943098453c60590239a4c811006e396f45
Nathan <zhaody.mu@gmail.com>
2024-08-25 21:47:12 +1000

    printed app version to the console

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { APP_PORT } from './config.ts';
 import routes from './routes/index.ts';
+import packageJson from '../package.json';
 
 const app = express();
 
@@ -8,4 +9,5 @@ app.use('/', routes);
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port ${APP_PORT}`);
+  console.log(`Auto.mate version: ${packageJson.version}`);
 });
